120127689a02bee150ce5ea0c6a249b7fe48b3e3
Jason Hsu <jhsu802701@jasonhsu.com>
2017-09-30 05:11:40 +0000

    Added omniauth authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920220301) do
+ActiveRecord::Schema.define(version: 20170930045921) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20170920220301) do
     t.string "last_name"
     t.string "first_name"
     t.string "username"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
